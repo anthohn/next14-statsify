01e72719ef6d52f0b163bd3cb808f8e0e28e6624
Anthony Höhn <anthonyhohn@gmail.com>
2023-12-12 16:50:23 +0100

    fetching topArtists

--- app/TopArtists/page.tsx
+++ app/TopArtists/page.tsx
@@ -1,8 +1,58 @@
-export default function TopArtists() {
-   return (
-     <h1>
-        TopArtists
-     </h1>
+'use client'
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 
-    );
-  }
+function TopTracks() {
+  const { data: session } = useSession();
+  const [topArtists, setTopArtists] = useState([]);
+
+  useEffect(() => {
+    const getTopArtists = async () => {
+      if (!session) {
+        console.log("L'utilisateur n'est pas connecté");
+        return;
+      }
+
+      const token = session.accessToken; // Utilisez le token de la session
+
+      try {
+        const response = await fetch('https://api.spotify.com/v1/me/top/artists?time_range=short_term', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`API call failed with status ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Top Artists data:", data);
+        setTopArtists(data.items); // Mettre à jour l'état avec les artistes top
+      } catch (error) {
+        console.error("Error fetching top artists:", error);
+      }
+    };
+
+    getTopArtists();
+  }, [session]); // Déclenchez l'effet lors du changement de la session
+
+  return (
+    <div>
+      <h2>Top Artists</h2>
+      <ul>
+        {topArtists.map((artist) => (
+          <a key={artist.id}>
+            <div>
+              <img src={artist.images[0]?.url} alt={artist.name} style={{ width: "50px", height: "50px" }} />
+              <div>{artist.name}</div>
+            </div>
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TopTracks;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { getServerSession } from "next-auth";
-
 export default async function Page() {
   const session = await getServerSession();
 
   return (
     <>
-      
     </>
   );
 }
