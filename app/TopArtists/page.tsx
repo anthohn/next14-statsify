7f9f836cf03e323faa26f3bc5208f8f53e6c911d
Anthony Höhn <anthonyhohn@gmail.com>
2023-12-13 11:46:03 +0100

    Specifie Type & change <img> for <Image>

--- app/TopArtists/page.tsx
+++ app/TopArtists/page.tsx
@@ -1,20 +1,29 @@
-'use client'
+'use client';
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import Image from 'next/image';
+
+
+// Définition du type pour un artiste
+interface Artist {
+  id: string;
+  name: string;
+  images: Array<{ url: string }>;
+}
 
 function TopTracks() {
   const { data: session } = useSession();
-  const [topArtists, setTopArtists] = useState([]);
+  const [topArtists, setTopArtists] = useState<Artist[]>([]); // Utiliser le type Artist ici
   const [timeRange, setTimeRange] = useState('short_term'); // short_term, medium_term, long_term
 
   useEffect(() => {
     const getTopArtists = async () => {
-      if (!session) {
+      if (!session || !session.accessToken) {
         console.log("L'utilisateur n'est pas connecté");
         return;
       }
 
-      const token = session.accessToken; // Utilisez le token de la session
+      const token = session.accessToken;
 
       try {
         const response = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}&limit=30`, {
@@ -52,16 +61,15 @@ function TopTracks() {
           All time
         </button>
       </div> 
-      
 
-    <div className="flex flex-wrap justify-center">
-      {topArtists.map((artist, index) => (
-        <div key={artist.id} className="flex justify-center bg-white shadow-2xl hover:scale-105 transition w-80 m-8 h-80 rounded-xl flex-col space-y-4 items-center"> 
-            <img src={artist.images[0]?.url} alt={artist.name} className="w-56 h-56 rounded-xl" />
-            <p className="text-xl font-bold">{index + 1}. {artist.name}</p>
-        </div>
-      ))}
-    </div>
+      <div className="flex flex-wrap justify-center">
+        {topArtists.map((artist: Artist) => (
+          <div key={artist.id} className="flex justify-center bg-white/70 shadow-2xl hover:scale-105 transition w-80 m-8 h-80 rounded-xl flex-col space-y-4 items-center"> 
+              <Image src={artist.images[0]?.url} alt={artist.name} width={224} height={224} className="rounded-xl" />
+              <p className="text-xl font-bold">{artist.name}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
