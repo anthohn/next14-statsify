a54572508b361c2ed8357e60eb79c839c3e5a711
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-12-16 19:30:57 +0100

    navbar link fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -36,10 +36,12 @@ export default function Header() {
   return (
     <nav className="z-[999] md:fixed bg-white md:left-1/2 md:-translate-x-1/2 md:mx-auto md:rounded-2xl shadow-2xl md:w-11/12 max-w-screen-xl md:my-4 mx-auto sm:px-6">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <Image src={'/logo.png'} width={100} height={100} style={{ width: 30, height: 'auto'}} alt="logo" className="" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Statsify</span>
-        </a>
+        <Link 
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+          href="/">
+          <Image src={'/logo.png'} width={100} height={100} style={{ width: 30, height: 'auto'}} alt="logo" className="" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Statsify</span>
+        </Link>
         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
