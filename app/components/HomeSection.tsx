a548a6aa42473e15b2accf1f2df39fdabf410165
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-12-16 22:19:53 +0100

    correct footer

--- app/components/HomeSection.tsx
+++ app/components/HomeSection.tsx
@@ -4,11 +4,11 @@ import HomeButton  from "@/app/components/HomeButton";
 export default async function HomeSection() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full mx-auto mt-36 py-20 bg-neutral-50/70 rounded-2xl shadow-2xl">
+      <div className="flex flex-col items-center justify-center w-full mx-auto mt-12 sm:mt-44 py-12 bg-neutral-50/70 rounded-2xl shadow-2xl">
         <h1 className="text-4xl mb-6 font-semibold text-center">Personal Spotify Statistics</h1>
         <HomeButton />
       </div>
-      <div className="felx flex-col space-y-8 mx-auto">
+      <div className="felx flex-col space-y-8 mx-auto pb-48">
         <div className="flex space-x-10 items-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"  viewBox="0 0 16 16"><path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/></svg>            
           <div className="felx flex-col space-y-4 max-w-xs">
